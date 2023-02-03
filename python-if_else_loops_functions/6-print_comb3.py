7f4b42ba5b05b7e0be0c641420860c3504dc8538
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-02-03 10:20:27 -0800

    inventing is a combination of brains and materials

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,8 @@
 for i in range(0, 10):
     for w in range(i + 1, 10):
         if i == 8 and w == 9:
-            print(f"{i}{w}")
+            print("{}{}".format(i, w))
         else:
-            print(f"{i}{w}", end=", ")
+            print("{}{}".format(i, w), end=", ")
 
 
