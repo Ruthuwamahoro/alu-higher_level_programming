2e59f8c28a7fe859f4f362ec75a5ef92ca0abe75
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-02-22 11:25:17 -0800

    secure acceee to an element in a list

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if 0 <= idx :
-        return my_list[idx]
-    elif idx < len(my_list) :
-        return my_list[idx]
-    else:
+    if idx < 0 :
         return None
+    elif idx > len(my_list) :
+        return None
+    else:
+        return my_list[idx]
     
 
         
