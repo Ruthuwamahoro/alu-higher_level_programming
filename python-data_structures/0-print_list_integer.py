a0a1a7d1cbf5228d6ec975b9e5c9cdfa119105ff
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-02-22 04:14:50 -0800

    Print all intergers of a list

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__=__main__:
+my_list = [1, 2, 3, 4, 5] 
+    def print_list_integer(my_list=[]):
+        print("{:d}.format(my_list))
+    print_list_integer(my_list=[]    
+
