fecc812c5688bd3322d4a664a245b22f5ed24af5
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-02-23 07:44:26 -0800

    only by 2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in mylist:
-        if i*2:
-            return True
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            new_list.append(True)
         else:
-            return False
+            new_list.append(False)
+    return new_list
+
