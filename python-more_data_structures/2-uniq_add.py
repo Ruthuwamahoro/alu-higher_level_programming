e90270217be61ee97e030d01da487a046d617a1e
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-02-26 14:01:21 -0800

    tasks

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    unique_integers = []
-    for num in my_list:
-        if ele not in unique_integers:
-            unique_integers.append(ele)
-    return sum(unique_integers)    
+    unique_integers = list()
+    for inte in my_list:
+        if inte not in unique_integers:
+            unique_integers.append(inte)
+    return sum(unique_integers)
