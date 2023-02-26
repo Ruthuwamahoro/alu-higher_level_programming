699932daaaba1f5030f13e9d97fe812b8e097c84
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-02-26 13:24:16 -0800

    tasks

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    return sum(set(my_list))    
+    unique_integers = []
+    unique_numbers = set(my_list)
+    for num in unique_numbers:
+        unique_integers.append(num)
+    return sum(unique_integers)    
