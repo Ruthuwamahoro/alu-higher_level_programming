520df3b85db43007f77a3b57526111bbe09caf4b
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-03 11:27:54 -0800

    print a list of integers

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    new_list=my_list.reverse()
-    print("{:d}".format(new_list))
+    if my_list:
+        my_list.reverse()
+
+    for list in range(len(my_list)):
+        print("{:d}".format(list))
