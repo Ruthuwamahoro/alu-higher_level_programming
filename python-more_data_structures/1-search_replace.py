a1c172a10a434061ba8da16e97c0e49c5f754142
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-05 06:27:29 -0800

    tasks

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for element in my_list:
-        if element != search:
-            return element
-        else:
-            return replace
+    return [element if element != search else replace for element in my_list]
+
