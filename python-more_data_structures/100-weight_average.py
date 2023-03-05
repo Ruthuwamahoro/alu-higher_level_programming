2716168a5cccda98e03f5ee1a7e21bcde6742532
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-05 07:15:09 -0800

    tasks

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if len(my_list) == 0:
+    if my_list:
         return 0
-    return sum([x*y for (x, y) in my_list]) / sum([y for (x, y) in my_list])
+    return sum([n*m for (n, m) in my_list]) / sum([m for (n, m) in my_list])
