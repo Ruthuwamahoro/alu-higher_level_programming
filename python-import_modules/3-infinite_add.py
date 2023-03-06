2a5aa4f87ea2420cb808db5e4ea7fc04c161268e
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-06 07:13:49 -0800

    change

--- python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__="__main__":
-    from sys import argv
-    num=0
-    x=len(argv)
-    for i in (1,x):
-        num += int(argv[i])
-    print("{:d}".format(num))    
