aaa33e1966ea667334f388e8c36a70511dc9cfb1
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-06 08:14:52 -0800

    tasks

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-if __name__=="__main__":
+if __name__ == "__main__":
     import sys
-    num=0
-    x=len(sys.argv)
-    for i in range(1,x):
+    num = 0
+    x = len(sys.argv)
+    for i in range(1, x):
         num += int(sys.argv[i])
     print("{:d}".format(num))
