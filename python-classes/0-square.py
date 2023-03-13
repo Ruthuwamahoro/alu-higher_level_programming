36bab2cfe7d422de3980abc725f3f1d9c8a0ece0
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-13 03:46:42 -0700

    My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+""" we don't have to import any module"""
+
+class Square:
+    """Square is the product of a number multiplied by itself"""
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,1 @@
+This directory contains the codes and all the things about classes as an object oriented programming
