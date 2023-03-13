c4e5eca219930536c47d9a386c93f17cb2e3c930
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-13 06:33:50 -0700

    fourth commit

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,14 +5,14 @@
 
 class Square:
     """Square is the product of a number multiplied by itself"""
-    def __init__(self, __size = 0):
+    def __init__(self, size=0):
 
         """ initialization of square class"""
         if not isinstance(size, int):
 
             raise TypeError('size must be an integer')
 
-        elif __size < 0 :
+        elif size < 0 :
 
             raise ValueError('size must be >= 0')
 
