6b1ba8c2cded709d356a80407de1d9245ee3105d
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-13 06:12:53 -0700

    this is the first commit for second question about size validation

---
+++ python-classes/2-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" we don't have to import any module"""
+
+
+class Square:
+    """Square is the product of a number multiplied by itself"""
+    def __init__(self, __size = 0):
+        self.__size = __size
+        if __size is int:
+            pass
+        elif __size is not int:
+            raise TypeError('size must be an integer')
+        elif __size < 0 :
+            raise ValueError('size must be >= 0')
+            
+       
+
+
+
+
+
+
+
+        
+
