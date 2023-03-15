f28f923287635052635260ee1c4f7f9b99a7e3e9
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-15 01:02:03 -0700

    making pep8 validation

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,18 +2,20 @@
 
 """we are not allowed to import any module so this module defines square"""
 
+
 class Square:
+
     """This class is called square"""
+
     def __init__(self, size=0):
-        """initialization of class attribute"""    
+        """initialization of class attribute"""
         self._size = size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        if size < 0 :
+        if size < 0:
             raise ValueError('size must be >= 0')
-        
+
     def area(self):
         """calculation of square area"""
         return (self._size ** 2)
-    
 
