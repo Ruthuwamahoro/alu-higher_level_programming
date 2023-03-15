3189e25d8a5c03ede67f11d773c271709fef7e01
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-15 00:44:13 -0700

    finding area of square class

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""we are not allowed to import any module so this module defines square"""
+
+class Square:
+    """This class is called square"""
+    def __init__(self, size=0):
+        """initialization of class attribute
+        Raises:
+            TypeError with message size must be an integer if size is not an integer
+            ValueError if size is less than 0
+        """    
+        self._size=size
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0 :
+            raise ValueError('size must be >= 0')
+        def area(self):
+            """calculation of square area"""
+            return (self._size ** 2)
+    
+
