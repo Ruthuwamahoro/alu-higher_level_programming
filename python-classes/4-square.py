92e49e4cf7111926024ec345d9b3a44d36f6cfd7
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-15 05:42:21 -0700

    accessing and updatimg attribute

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,12 +6,14 @@ class Square:
     """this is a square class"""
     def __init__(self, size=0):
         """initialization of instance attribute"""
-        self.__size=size
+        
 
         if not isinstance(size, int):
             return TypeError('size must be an integer')
         if size < 0:
             return ValueError('size must be >= 0')
+        self.__size=size
+
 
     @property
     def size(self):
@@ -20,11 +22,13 @@ class Square:
     @size.setter
     def size(self, value):
         """setting value of size"""
-        self.__size=value
+        
         if not isinstance(size, int):
             return TypeError('size must be an integer')
         if size < 0:
             return ValueError('size must be >= 0')
+         self.__size = value
+
 
     def area(self):
         """calculating square area"""
