df80299208f767052e6af83a66ad9126beb21c8f
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-15 00:49:33 -0700

    making indetation on method

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -5,18 +5,15 @@
 class Square:
     """This class is called square"""
     def __init__(self, size=0):
-        """initialization of class attribute
-        Raises:
-            TypeError with message size must be an integer if size is not an integer
-            ValueError if size is less than 0
-        """    
-        self._size=size
+        """initialization of class attribute"""    
+        self._size = size
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0 :
             raise ValueError('size must be >= 0')
-        def area(self):
-            """calculation of square area"""
-            return (self._size ** 2)
+        
+    def area(self):
+        """calculation of square area"""
+        return (self._size ** 2)
     
 
