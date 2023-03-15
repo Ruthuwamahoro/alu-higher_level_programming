417c12a59eae072fc7a8d3d1ded28d0a3957666e
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-15 08:35:35 -0700

    printing a square

---
+++ python-classes/5-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""module that defines a square"""
+
+class Square:
+    """representation of square class"""
+    def __init__(self, size=0):
+        """initialization of attribute"""
+        self.__size=size
+        if not isinstance(size, int):
+            return TypeError('size must be an integer')
+        if size < 0:
+            return ValueError('size must be >= 0')
+    @property
+    def size(self):
+        """getting value of size of square"""
+        return self.__size
+    @size.setter
+    def size(self, value):
+        """setting value"""
+        if not isinstance(size, int):
+            return TypeError('size must be an integer')
+        if size < 0:
+            return ValueError('size must be >=0')
+        self.__size = value
+
+    def area(self):
+        """return curent square area"""
+        return self.__size * self.__size
+    def my_print(self):
+        """prints in stdout with the character #"""
+        if self.__size == 0:
+            print()
+        for n in range(self.__size):
+            print("#" * self.__size)
