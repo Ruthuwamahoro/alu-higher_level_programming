0be1f82245acf93cacb8885f845a41671250c4e4
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-17 02:28:31 -0700

    debugging

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-# 3-rectangle.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Defines a Rectangle class."""
+"""we do not need to import any module just defines a Rectangle class in this current module."""
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """creating a class called Rectangle."""
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the new rectangle.
-            height (int): The height of the new rectangle.
-        """
+        """Initialization of new instances."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """Get/set the width of the Rectangle."""
+        """retrieving the value of  the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -32,7 +25,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """Get/set the height of the Rectangle."""
+        """retrieving the value of  height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -44,20 +37,17 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """Return the area of the Rectangle."""
+        """calculating and returning  the area of the Rectangle."""
         return (self.__width * self.__height)
 
     def perimeter(self):
-        """Return the perimeter of the Rectangle."""
+        """calculating and Returning  the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return ((self.__width * 2) + (self.__height * 2))
 
     def __str__(self):
-        """Return the printable representation of the Rectangle.
-
-        Represents the rectangle with the # character.
-        """
+        """Return the string presentation"""
         if self.__width == 0 or self.__height == 0:
             return ("")
 
