94fbd86b09799185b21bcaa093cfac4925e72f1b
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-20 07:31:02 -0700

    changes

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""module that defines a class BaseGeometry"""
+""" New class Geometry module """
 
 
 class BaseGeometry:
-    """Representation of BaseGeometry class"""
+    """ Base Geometry """
 
     def area(self):
         raise Exception("area() is not implemented")
