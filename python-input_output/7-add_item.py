f1c031f3627a8eb0a6abccec2d573c6f11fda2fb
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-20 10:15:47 -0700

    load add and save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
-"""script that adds all arguments to list"""
+# script that adds all arguments to a Python list,
+# and then save them to a file
+"""
+    script that adds all arguments to a Python list,
+    and save them to a file
+"""
+
 import sys
 
 if __name__ == "__main__":
