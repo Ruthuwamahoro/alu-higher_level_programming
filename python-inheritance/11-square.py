e78f6684fe6b82593bdc13c70bc72fe469b79eca
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-20 08:20:01 -0700

    square

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a Rectangle base of Square."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Representing a square as class."""
+
+    def __init__(self, size):
+        """Initialization of new instances of square"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size        
