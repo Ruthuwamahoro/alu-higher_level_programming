dc5bfcc944ddd3c05e79823853d067e9010051d4
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-20 04:17:54 -0700

    my list

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class that Mylist that inherits from list"""
+
+
+class Mylist(List):
+    """print list in sorted order"""
+
+    def print_sorted(self):
+        print(sorted(self))
