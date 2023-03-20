5e01f44b7d016907b55cebdd6fd6726fbe001ad5
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-20 03:42:11 -0700

    looksup

---
+++ python-inhertance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that return list of available attribute"""
+
+
+def lookup(obj):
+    """return list of available attribute"""
+    list = []
+    for n in dir(obj):
+        return list.append(n)
