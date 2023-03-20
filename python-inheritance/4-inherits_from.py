c5ceb13dec2467801941f7966c0955d419aa73e9
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-20 06:35:04 -0700

    changes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
 """function that return True if an object
-is an instance of class that inherited
-directly or indirectly from
-specific class"""
+is an instance of class that inherited"""
 
 
 def is_kind_of_class(obj, a_class):
-    """through implementation
-    Args:
-        obj(any):object to check
-        a_class:type of class to match
-    Returns:
-        True if obj is an instanceof class
-        inherited directly or directly
-    """
+    """through implementationed"""
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
