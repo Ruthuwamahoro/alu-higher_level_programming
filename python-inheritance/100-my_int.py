18f26493f64dff1787e1334b9dc889a863a4a997
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-24 11:57:01 -0700

    changes

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+# 100-my_int.py
+# Brennan D Baraban <375@holbertonschool.com>
 """Defines a class MyInt that inherits from int."""
 
 
@@ -6,9 +8,9 @@ class MyInt(int):
     """Invert int operators == and !=."""
 
     def __eq__(self, value):
-        """Override == opeartor with != behavior."""
+        """Override == operator with != behavior."""
         return self.real != value
 
     def __ne__(self, value):
         """Override != operator with == behavior."""
-        return self.real == value    
+        return self.real == value
