db5aa74e482a82332fe10396ba95171fd7ff220a
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-03-29 12:14:10 -0700

    list all the cities of califonia

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,6 @@
+-- List all the cities of califonia
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+    AND states.name = 'California'
+    ORDER BY cities.id ASC;
