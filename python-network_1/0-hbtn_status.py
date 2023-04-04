066e414e922938315d15234c1fad0f511223a3c0
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-04-04 10:42:46 -0700

    fetching urllib

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """fetching data from URL"""
 import urllib.request
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as re:
-    data = re.read()
+
+if __name__ == "__main__":
+    y = urllib.request.Request("https://intranet.hbtn.io/status")
+    with urllib.request.urlopen(y) as re:
+        data = re.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(data)))
+        print("\t- content: {}".format(data))
+        print("\t- utf8 content: {}".format(data.decode("utf-8")))
