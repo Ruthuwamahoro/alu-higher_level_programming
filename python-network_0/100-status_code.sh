442351a2f5ae0c43ab1e93409d785d934a233657
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-04-07 09:44:28 +0000

    changing comment

--- python-network_0/100-status_code.sh
+++ python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#bash that makes a request 
+#bash that sends a request to a URL and displays only status code 
 curl -s -o /dev/null -w "%{http_code}" "$1"
--- python-network_0/2-delete.sh
+++ python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Delete request to the URL 
+# Delete request to the URL and displays the body of response
 curl -sX DELETE "$1"
