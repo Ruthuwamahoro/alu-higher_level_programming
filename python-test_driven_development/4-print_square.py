440ed1450aa8a3f65c341769b070470cfc85420f
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-04-13 06:33:51 -0700

    pep8 validation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """defining a function that prints a square with character"""
+
+
 def print_square(size):
     """Print a square with the character.
     Args:
@@ -13,6 +15,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for i in range(size):
-        [print("#", end="") for i in range(size)]
+    for n in range(size):
+        [print("#", end="") for n in range(size)]
         print("")
