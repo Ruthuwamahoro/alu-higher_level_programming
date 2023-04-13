14cfbfdefeb08db2236bd6951817fe44ca92cf2e
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-04-13 06:01:44 -0700

    changes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,8 +4,9 @@
 
 def add_integer(a, b=98):
     """function that add two integers"""
+
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
     if type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)    
+return int(a) + int(b)
