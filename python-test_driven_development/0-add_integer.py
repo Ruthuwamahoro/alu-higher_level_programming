8908d274f40b7515eb2a4b785df81cd5257a3860
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-04-13 06:03:49 -0700

    changes

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -7,6 +7,6 @@ def add_integer(a, b=98):
 
     if type(a) not in [int, float]:
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    elif type(b) not in [int, float]:
         raise TypeError("b must be an integer")
-return int(a) + int(b)
+    return int(a) + int(b)
