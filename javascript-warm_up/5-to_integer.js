ad5c91ef119b401c021f4df96157b0389a959c42
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-07 14:38:22 -0700

    printing integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const passingValue = parseInt(process.argv[2]);
 if (!isNaN(passingValue)) {
-  console.log(`My number:${passingValue}`);
+  console.log(`My number: ${passingValue}`);
 } else {
   console.log('Not a number');
 }
