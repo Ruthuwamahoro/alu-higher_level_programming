66ddebabe59b82ae9d3c7c9189561a290ff3a254
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-08 04:29:21 -0700

    adding numbers

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-function add(a=3,b=5){
-	const sum = a+b;
-	return sum;
+function add (a, b) {
+  return a + b;
 }
-const a = 10;
-const b = 7;
-const answer = add(a, b);
-console.log(`${answer}`);
 
+module.exports = {
+  add: add
+};
