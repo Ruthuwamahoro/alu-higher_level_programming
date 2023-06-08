3a6260ad403e69b3859f10e77d6279e490e1c00d
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-08 06:21:51 -0700

    adding a number

--- javascript-warm_up/100-let_me_const.js
+++ javascript-warm_up/100-let_me_const.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
-
-function updateMyVar() {
+function updateMyVar () {
   myVar = 333;
 }
 updateMyVar();
---
+++ javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+function executeXTimes (x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+function myFunction() {
+  console.log("Executing the function");
+}
+
+executeXTimes(5, myFunction);
