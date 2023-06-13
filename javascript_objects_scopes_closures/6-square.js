cc8bd9d9739a7c5f6ec498215b59a186563ec026
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-13 00:11:41 -0700

    done

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,10 +2,9 @@
 module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-	    console.log(c.repeat(this.width));
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 };
