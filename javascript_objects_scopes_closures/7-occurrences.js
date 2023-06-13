579a19f5acd237a2a8549063395c21f7ab38cbdd
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-12 23:37:38 -0700

    adding

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-exports.nbOccurrences = function(list, searchElement) {
-  let count = 0;
-
-  for (let i = 0; i < list.length; i++) {
-    if (list[i] === searchElement) {
-      count += 1;
-    }
-  }
-
-  return count;
+exports.nbOccurences = function (list, searchElement) {
+  return list.filter((n) => n === searchElement).length;
 };
