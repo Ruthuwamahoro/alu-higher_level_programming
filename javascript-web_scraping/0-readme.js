6fafa65f94b3a444d9e44ac9d870036a750255ef
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-14 07:54:49 +0000

    reading and printing file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    if (err.code === 'ENOENT') {
-      console.error(`${filePath} doesn't exist`);
-    } else {
-      console.error(err);
-    }
-  }
-
-  console.log(`File content:\n${data}`);
+fs.readFile(filePath, 'utf8', (err, data) =>{
+    console.log(err || data);
 });
