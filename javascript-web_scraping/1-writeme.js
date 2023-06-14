739286959d2596680e0436bdc78cd5f0e52019ac
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-14 08:18:02 +0000

    write to the file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const filePath = process.argv[2];
 const stringToWrite = process.argv[3];
-fs.writeFile(filePath, stringToWrite,'utf8',function(err){
-    if (err){
-        console.error(error);
-    }
+fs.writeFile(filePath, stringToWrite, 'utf8', err => {
+  if (err) {
+    console.log(err);
+  }
 });
