e5a489a748229ebd6a8718f4898537d01f4153ea
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-14 07:30:57 +0000

    reading and printing a file

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
 const fs = require('fs');
 const filePath = process.argv[2];
+if (!filePath) {
+  console.error("no such file or directory");
+}
 fs.readFile(filePath, 'utf8', (err, data) => {
   if (err) {
     console.err(err);
