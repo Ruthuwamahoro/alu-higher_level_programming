69cf60dad3ed4a7539caf1a22dcfb0b666c6a7cb
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-14 09:38:53 +0000

    request

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
-request.get(url,(error,response) => {
-    if(error){
-        console.log(error)
-    }
-    console.log(JSON.parse(response.body).title);
+const url = process.argv[2];
+request.get(url).on('response',function (response) {
+    console.log(`code: ${response.statusCode}`);
 });
