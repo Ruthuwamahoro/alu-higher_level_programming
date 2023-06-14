3fb02cb631298c618636e783c08c9a1d7f1fefc9
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-14 10:11:05 +0000

    request

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -3,12 +3,12 @@ const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/';
 const characterId = 18;
 request.get(url, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    }
-    const films = JSON.parse(body);
-    const moviesWithWedge = films.results.filter(film =>
-        film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    );
-    console.log(moviesWithWedge);
+  if (error) {
+    console.log(error);
+  }
+  const films = JSON.parse(body);
+  const moviesWithWedge = films.results.filter(film =>
+    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
+  );
+  console.log(moviesWithWedge);
 });
