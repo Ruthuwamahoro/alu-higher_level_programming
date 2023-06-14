406067682cf9299eb3ee6f8bd11c821057d882df
Ruthuwamahoro <r.uwamahoro@alustudent.com>
2023-06-14 10:52:23 +0000

    finally

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
-const url = process.argv[2];
-request(url, function (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const todos = JSON.parse(body);
-    const tasksCompleted = {};
+    const completed = {};
     todos.forEach((todo) => {
-      if (todo.tasksCompleted && tasksCompleted[todo.userId] === undefined) {
-        tasksCompleted[todo.userId] = 1;
-      } else if (todo.tasksCompleted) {
-        tasksCompleted[todo.userId] += 1;
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-    console.log(tasksCompleted);
+    console.log(completed);
   }
 });
